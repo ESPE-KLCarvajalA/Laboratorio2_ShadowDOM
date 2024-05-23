9440dcf1f4ebaffc5389dde75f590719eb8f03c0
mscastellano <105226970+mscastellano@users.noreply.github.com>
2024-05-23 11:49:37 -0500

    estructura DOM

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    class HeaderElement extends HTMLElement {
+        constructor() {
+            super();
+            const shadow = this.attachShadow({ mode: 'open' });
+        }
+    }
+    
+})
