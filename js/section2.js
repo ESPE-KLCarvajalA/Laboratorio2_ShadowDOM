981eebba7d05c0ef64db213df6e95e35b77cd369
Brithney Salazar <93927947+Brithn@users.noreply.github.com>
2024-05-23 11:44:06 -0500

    Index  y secciones

---
+++ js/section2.js
@@ -0,0 +1,20 @@
+
+class GalleryElement extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = `
+            
+            <div class="gallery">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 1">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 2">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 3">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 4">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 5">
+                <img src="https://via.placeholder.com/200" alt="Gallery Image 6">
+            </div>
+        `;
+    }
+}
+
+customElements.define('gallery-element', GalleryElement);
